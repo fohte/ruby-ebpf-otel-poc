9a8142f38154fbeab49bc48cae8d6cbca6412c81
Hayato Kawai <fohte.hk@gmail.com>
2025-01-19 17:15:20 +0900

    trace yjit compilation

--- src/poc.py
+++ src/poc.py
@@ -1,11 +1,11 @@
 from bcc import BPF
 import os
 
-b = BPF(src_file=os.path.join(os.path.dirname(__file__), "poc.bpf.c")
+b = BPF(src_file=os.path.join(os.path.dirname(__file__), "poc.bpf.c"))
 
 b.attach_uprobe(
-    name="/home/ec2-user/.rbenv/versions/3.4.1/bin/ruby",
-    sym="rb_io_puts",
+    name="/home/ec2-user/.rbenv/versions/3.4.1/lib/libruby.so.3.4",
+    sym="rb_yjit_iseq_gen_entry_point",
     fn_name="trace_puts",
 )
 
