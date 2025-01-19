8db2b552f89dc8a6d803d01041b5a9873962784e
Hayato Kawai <fohte.hk@gmail.com>
2025-01-19 20:56:10 +0900

    GC & YJIT metrics

--- src/poc.bpf.c
+++ src/poc.bpf.c
@@ -1,53 +1,28 @@
-int trace_rb_method_entry(struct pt_regs *ctx) {
-  bpf_trace_printk("method called [usdt]\n");
-  return 0;
-}
+#include <uapi/linux/ptrace.h>
 
-int trace_rb_method_return(struct pt_regs *ctx) {
-  bpf_trace_printk("method returned [usdt]\n");
-    char class_name[128] = {};
-    char method_name[128] = {};
+enum metrics_t {
+  GC = 0,
+  YJIT = 1,
+};
 
-    u64 class_ptr = 0;
-    u64 method_ptr = 0;
-
-    bpf_usdt_readarg(1, ctx, &class_ptr);
-    bpf_usdt_readarg(2, ctx, &method_ptr);
-
-    if (class_ptr != 0) {
-        bpf_probe_read_str(&class_name, sizeof(class_name), (void *)class_ptr);
-    }
-    if (method_ptr != 0) {
-        bpf_probe_read_str(&method_name, sizeof(method_name), (void *)method_ptr);
-    }
-
-    bpf_trace_printk("Method returned: %s", class_name);
-    bpf_trace_printk("#%s\n", method_name);
-
-  return 0;
-}
-
-int trace_rb_cmethod_entry(struct pt_regs *ctx) {
-  bpf_trace_printk("cmethod called [usdt]\n");
-  return 0;
-}
-
-int trace_rb_cmethod_return(struct pt_regs *ctx) {
-  bpf_trace_printk("cmethod returned [usdt]\n");
-  return 0;
-}
+BPF_HASH(metric_map, u32, u64);
 
 int trace_rb_gc_usdt(struct pt_regs *ctx) {
-  bpf_trace_printk("GC happened [usdt]\n");
+  u32 key = GC;
+  u64 *count, zero = 0;
+
+  count = metric_map.lookup_or_init(&key, &zero);
+  (*count)++;
+
   return 0;
 }
 
-int trace_rb_gc(struct pt_regs *ctx) {
-  bpf_trace_printk("GC happened\n");
-  return 0;
-}
+int trace_rb_yjit(struct pt_regs *ctx) {
+  u32 key = YJIT;
+  u64 *count, zero = 0;
 
-int trace_rb_puts(struct pt_regs *ctx) {
-  bpf_trace_printk("puts called\n");
+  count = metric_map.lookup_or_init(&key, &zero);
+  (*count)++;
+
   return 0;
 }
--- src/poc.py
+++ src/poc.py
@@ -1,14 +1,11 @@
 from bcc import BPF, USDT
+from time import sleep
 import os
 
 pid = int(os.popen("pidof ruby").read().strip())
 
 usdt = USDT(pid=pid)
 usdt.enable_probe(probe="gc__mark__begin", fn_name="trace_rb_gc_usdt")
-usdt.enable_probe(probe="method__entry", fn_name="trace_rb_method_entry")
-usdt.enable_probe(probe="method__return", fn_name="trace_rb_method_return")
-# usdt.enable_probe(probe="cmethod__entry", fn_name="trace_rb_cmethod_entry")
-# usdt.enable_probe(probe="cmethod__return", fn_name="trace_rb_cmethod_return")
 
 b = BPF(
     src_file=os.path.join(os.path.dirname(__file__), "poc.bpf.c"),
@@ -17,15 +14,22 @@ b = BPF(
 
 b.attach_uprobe(
     name="/home/ec2-user/.rbenv/versions/3.4.1/lib/libruby.so.3.4",
-    sym="rb_gc_start",
-    fn_name="trace_rb_gc",
-)
-
-b.attach_uprobe(
-    name="/home/ec2-user/.rbenv/versions/3.4.1/lib/libruby.so.3.4",
-    sym="rb_io_puts",
-    fn_name="trace_rb_puts",
+    sym="rb_yjit_iseq_gen_entry_point",
+    fn_name="trace_rb_yjit",
 )
 
 print("Tracing... Hit Ctrl-C to end.")
-b.trace_print()
+
+metric_keys = {0: "GC", 1: "YJIT"}
+
+print("Metrics:")
+
+try:
+    while True:
+        metric_map = b["metric_map"]
+        for k, v in metric_map.items():
+            metric_key = metric_keys[k.value]
+            print(f"{metric_key}: {v.value}\n")
+        sleep(1)
+except KeyboardInterrupt:
+    print("Exiting...")
